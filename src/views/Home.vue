4583c4392251d3e7117b3c459fde75905c86cf1b
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-02-02 18:46:32 +0700

    construct toolbar and history viewing template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class='home'>
+  <div class="home">
       <v-toolbar dense :dark="true">
-        <v-spacer></v-spacer>
         <v-toolbar-title>News for you</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>mdi-heart</v-icon>
+        <v-btn @click="showHistoryModal">
+          <v-icon left>mdi-history</v-icon>
+          History
         </v-btn>
       </v-toolbar>
       <v-content>
@@ -32,6 +32,76 @@
                   </v-card>
                 </v-col>
             </v-row>
+    <v-dialog
+      v-model="editing"
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="headline">Edit headline</v-card-title>
+        <v-divider :inset="inset"></v-divider>
+        <v-card-text>
+        <v-textarea
+          v-model="headline"
+          rows="1"
+          :hint="editHint"
+          :class="textAreaClass"
+          auto-grow
+          persistent-hint
+        ></v-textarea>
+        </v-card-text>
+        <v-divider :inset="inset"></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="red darken-1"
+            text
+            @click="closeEditModal"
+          >
+            Cancel
+          </v-btn>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="confirmEditModal"
+          >
+            Confirm
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      v-model="showHistory"
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="history">View history</v-card-title>
+        <v-divider :inset="inset"></v-divider>
+        <v-card-text v-for="(item, index) in history" v-bind:key="index">
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="list-title">
+                {{articles[item.index].title}}
+              </v-list-item-title>
+              <v-list-item-subtitle>{{item.time}}</v-list-item-subtitle>
+             </v-list-item-content>
+          </v-list-item>
+          <v-divider :inset="inset"></v-divider>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="red darken-1"
+            text
+            @click="closeHistoryModal"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
         </v-container>
       </v-content>
   </div>
@@ -44,7 +114,15 @@
     font-weight: bold;
     height: 90px;
     overflow: hidden;
-    text-overflow: ellipsis;
+  }
+  .edit-headline {
+    font-weight: bold;
+  }
+  .edit-headline.length-error {
+    background-color:#ffcccb;
+  }
+  .list-title {
+    font-weight: bold;
   }
 </style>
 
@@ -68,10 +146,45 @@ export default {
       title: 'Grammys 2020: Billie Eilish Wins Best Pop Vocal Album - Pitchfork',
       urlToImage: 'https://media.pitchfork.com/photos/5e2a771c6fa96c0008414a29/2:1/w_790/Billie-Eilish-2.jpg',
     }],
+    history: [{ index: 0, time: '17.32' }, { index: 1, time: '17.33' }, { index: 2, time: '17.34' }, { index: 0, time: '17.35' }],
+    editing: false,
+    editingIndex: 0,
+    headline: '',
+    showHistory: false,
   }),
   methods: {
-    editHeader: (index) => {
-      alert(index);
+    editHeader(index) {
+      this.headline = this.articles[index].title;
+      this.editingIndex = index;
+      this.editing = true;
+    },
+    closeEditModal() {
+      this.editing = false;
+    },
+    confirmEditModal() {
+      if (this.headline.length > 150) {
+        return;
+      }
+      this.articles[this.editingIndex].title = this.headline;
+      this.editing = false;
+    },
+    showHistoryModal() {
+      this.showHistory = true;
+    },
+    closeHistoryModal() {
+      this.showHistory = false;
+    },
+  },
+  computed: {
+    textAreaClass() {
+      let outClass = 'edit-headline';
+      if (this.headline.length > 150) {
+        outClass += ' length-error';
+      }
+      return outClass;
+    },
+    editHint() {
+      return `${this.headline.length}/150 characters`;
     },
   },
 };
